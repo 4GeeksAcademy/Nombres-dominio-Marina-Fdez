b3be3ea4ea86e279626217abde90b8c3f0b09c07
Marinafdez-o <mfz.oria@gmail.com>
2025-07-25 10:50:23 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,29 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+
+window.onload = () => {
+  let pronombre = ['the', 'our'];
+  let adj = ['great', 'big'];
+  let nombre = ['jogger', 'racoon'];
+  let extension = ['.com', '.net', '.us', '.io'];
+
+  const lista = document.getElementById("lista-dominios");
+
+  for (let p of pronombre) {
+    for (let a of adj) {
+      for (let n of nombre) {
+        for (let ext of extension) {
+          let dominio = p + a + n + ext;
+
+          let item = document.createElement("li");
+          item.textContent = dominio;
+
+          
+          lista.appendChild(item);
+        }
+      }
+    }
+  }
 };
+
